259fc78e19b8a36e97ed76ef64882c1d2fc29043
yeung <734880084@qq.com>
2019-05-19 00:52:37 +0800

    原版

---
+++ src/containers/AddTodo.js
@@ -0,0 +1,14 @@
+import AddTodoForm from '../components/AddTodoForm'
+import {connect} from 'react-redux'
+import {addTodo} from "../actions";
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSubmit: (text) => {
+            dispatch(addTodo(text))
+        }
+    }
+};
+let AddTodo = connect(null, mapDispatchToProps)(AddTodoForm);
+
+export default AddTodo
